a9a16b88e11f558f563bf2ffcd9d4ad85d9349bd
SridharSSriram <sridhar3s17@gmail.com>
2020-06-09 01:23:38 -0400

    reorganized tings

--- zipcode.py
+++ zipcode.py
@@ -1,7 +1,7 @@
 import csv
 
 zip_lat_long_table= {}
-with open('zipcode-lat-long.csv') as csvfile:
+with open('data/zipcode-lat-long.csv') as csvfile:
     readCSV = csv.reader(csvfile, delimiter =',')
     next(readCSV, None)
     for row in readCSV:
